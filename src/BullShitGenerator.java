a097ba2bd96492db18308da9ba5b04c7512ad23f
gercike <gercike13@gmail.com>
2020-11-11 21:44:03 +0100

    bullshitgenerator is ready for now, starting the frontend part

--- src/BullShitGenerator.java
+++ src/BullShitGenerator.java
@@ -2,18 +2,23 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class BullShitGenerator {
+    char newLine = (char) 10;
+
+
     public static void main(String[] args) throws FileNotFoundException {
         ArrayList<String> text = Reader.readToArray("borbala.txt");
 //        System.out.println(Reader.wordsHashMap.keySet().size());
 //        System.out.println(SemAnalizer.findMaxWords(Reader.wordsHashMap));
         BullShitGenerator main = new BullShitGenerator();
-//        System.out.println(main.bullshitGenerator(text));
+        System.out.println(main.bullshitGenerator(text));
     }
 
     public String bullshitGenerator(ArrayList<String> words) {
         Random rndmzr = new Random();
         StringBuilder text = new StringBuilder();
         int dotSpot = 0;
+        boolean newRow = false;
+        int hadNewRow = 2;
         for (int i = 0; i < words.size(); ) {
             if (dotSpot == 0) {
                 dotSpot = rndmzr.nextInt(20) + 5; //hogy néha pont is kerüljön a szövegbe + ld. lejjebb
@@ -24,44 +29,50 @@ public class BullShitGenerator {
                 continue;
             } else {
                 String word = words.get(nr);
-                if (text.length() >= word.length() && word.equals(text.substring(text.length() - word.length() - 1, text.length() - 1))) { //hogy ne legyen szóismétlés
+                if (text.length() > word.length() && word.equals(text.substring(text.length() - word.length() - 1, text.length() - 1))) { //hogy ne legyen szóismétlés
                     continue;
                 }
-                if (text.length() >= word.length() && text.length() > 3 && isLastWordDefArticle_A_(text.substring(text.length() - 3, text.length())) &&
+                if (text.length() > 3 && isLastWordDefArticle_A_(text.substring(text.length() - 3, text.length())) &&
                         isLetterVowel(word.substring(0, 1))) { // 'a' névelő után ne kezdődjön szó magánhangzóval
                     continue;
                 }
-                if (text.length() >= word.length() && text.length() > 4 && isLastWordDefArticle_Az_(text.substring(text.length() - 4, text.length())) &&
+                if (text.length() > 4 && isLastWordDefArticle_Az_(text.substring(text.length() - 4, text.length())) &&
                         isLetterConsonant(word.substring(0, 1))) { // 'az' névelő után ne kezdődjön szó mássalhangzóval
                     continue;
                 }
                 nrsUsed.add(nr);
-                if (text.length() >= word.length() && word.equals("hogy") || word.equals("de") || word.equals("azonban")) {
-                    text.replace(text.length() - 1, text.length(), ", "); // a 'hogy', 'azonban' és 'de' szavak elé vessző kerül
-                }
-                if (text.length() == 0 || text.length() >= 2 && text.substring(text.length() - 2, text.length()).equals(". ")) {
+                if (text.length() == 0 || text.length() >= 2 && text.substring(text.length() - 2, text.length()).equals(". ") || newRow) {
                     word = firstLetterToUpperCase(word); //mondat első betűje nagybetű legyen
                 }
+                if (text.length() >= word.length() && wordNeedsComma(word)) {
+                    text.replace(text.length() - 1, text.length(), ", "); // a felsorolt szavak elé vessző kerül
+                }
                 text.append(word);
+                newRow = false;
+                int enter = rndmzr.nextInt(9);
                 if (dotSpot == 1) { //hogy néha pont is kerüljön a szövegbe
                     text.append(".");
+                    if (enter == 5 && hadNewRow == 0) { // időnként legyen sortörés, de ne soronként
+                        text.append(newLine);
+                        newRow = true;
+                    }
                 }
-                int enter = rndmzr.nextInt(10);
-                if (i != words.size() - 1) {        // a szöveg végén ne legyen se szóköz, se enter
-//                    if (enter == 8 || enter == 9) {
-//                        System.out.println((char) 13);
-//                    } else {
+                if (i != words.size() - 1) {  // a szöveg végén ne legyen se szóköz, se enter
                     text.append(" ");
-//                    }
+                }
+                if (i == words.size() - 1) {
+                    text.append(".");
                 }
                 i++;
                 dotSpot--;
+                if (hadNewRow > 0) {
+                    hadNewRow--;
+                }
             }
         }
         return text.toString();
     }
 
-    //a, á, e, é, i, í, o, ó, ö, ő, u, ú, ü, ű
     public boolean isLetterVowel(String letter) {
         switch (letter) {
             case "a":
@@ -129,7 +140,7 @@ public class BullShitGenerator {
     public String firstLetterToUpperCase(String word) {
         String firstLetter = word.substring(0, 1);
         if (word.length() > 1) {
-            String leftover = word.substring(1, word.length());
+            String leftover = word.substring(1);
             return firstLetter.toUpperCase() + leftover;
         } else {
             return firstLetter.toUpperCase();
@@ -145,6 +156,19 @@ public class BullShitGenerator {
 
     public boolean isLastWordDefArticle_Az_(String word) {
         if (word.equals(" az ") || word.equals(" Az ")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean wordNeedsComma(String word) {
+        if (word.equals("tehát") || word.equals("addig") || word.equals("amint") || word.equals("amik") || word.equals("mikor") || word.equals("melyet") || word.equals("minthogy") || word.equals("aztán") || word.equals(
+                "mintha") || word.equals(
+                "amely") || word.equals(
+                "akkor") ||
+                word.equals("majd") || word.equals("ami") || word.equals("mik") || word.equals("mit") || word.equals("mint") || word.equals("mert") || word.equals("hogy") ||
+                word.equals("de") || word.equals("azonban") || word.equals("akivel") || word.equals("amit") ||
+                word.equals("aki") || word.equals("hanem") || word.equals("ha") || word.equals("ahol") || word.equals("amikor") || word.equals("mely")) {
             return true;
         }
         return false;
---
+++ src/Frontend.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Frontend {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Üdvözli a szövegelemző és halandzsagyártó alkalmazás!");
+        System.out.print("Melyik szolgáltatásunk érdekli? A szövegelemzés (1) vagy a halandzsagyártás (2)? ");
+    }
+}
