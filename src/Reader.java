00f0f111c0a02ccfad0afe2bb60ef8c8ff514b4f
Mateskoda <skodamate93@gmail.com>
2020-11-11 15:03:57 +0100

    added read array

--- src/Reader.java
+++ src/Reader.java
@@ -6,10 +6,27 @@ public class Reader {
     public static HashMap<String, Integer> wordsHashMap = new HashMap<>();
 
     public static void main(String[] args) throws FileNotFoundException {
-        read("PairProject.From11.11/borbala.txt");
+        read("borbala.txt");
         System.out.println(wordsHashMap);
+        System.out.println(readToArray("borbala.txt"));
     }
+    public static ArrayList<String> readToArray(String textName) throws FileNotFoundException {
+        ArrayList<String> arrayListOfWords= new ArrayList<>();
+        Set<Character> charsToIgnore= new HashSet<>();
+        for (int i = 33; i <64 ; i++) {
 
+            charsToIgnore.add((char)i);
+        }
+        Scanner sc = new Scanner(new File(textName));
+
+        for (int i = 0; sc.hasNext(); i++) {
+            String word = sc.next();
+            word = word.substring(0).toLowerCase();
+            word = wordWithIgnores(word,charsToIgnore);
+            arrayListOfWords.add(word);
+
+        }return arrayListOfWords;
+    }
 
     public static HashMap<String, Integer> read(String textName) throws FileNotFoundException {
         Set<Character> charsToIgnore= new HashSet<>();
@@ -18,7 +35,7 @@ public class Reader {
             charsToIgnore.add((char)i);
         }
         Scanner sc = new Scanner(new File(textName));
-        
+
         for (int i = 0; sc.hasNext(); i++) {
             String word = sc.next();
 //            System.out.println(word);
@@ -35,17 +52,42 @@ public class Reader {
         }return wordsHashMap;
     }
 
+    public static HashMap<String, Integer> read2Words(String textName) throws FileNotFoundException {
+        Set<Character> charsToIgnore= new HashSet<>();
+        for (int i = 33; i <64 ; i++) {
+
+            charsToIgnore.add((char)i);
+        }
+        Scanner sc = new Scanner(new File(textName));
+        String word2 = sc.next();
+        for (int i = 0; sc.hasNext(); i++) {
+            String word = word2;
+//            System.out.println(word);
+            word2 =sc.next();
+            word = word.substring(0).toLowerCase();
+            word2 = word2.substring(0).toLowerCase();
+
+//            System.out.println(wordWithIgnores(word,charsToIgnore));
+
+            word = wordWithIgnores(word,charsToIgnore);
+            word2 = wordWithIgnores(word2,charsToIgnore);
+            word=word+ " " +word2;
+            wordsHashMap.putIfAbsent(word, 0);
+            Integer put = wordsHashMap.get(word);
+            put++;
+            wordsHashMap.put(word, put);
+        }return wordsHashMap;
+    }
 
 
     public static String wordWithIgnores(String toPrint, Set<Character> ignore) {
-       String word = "";
-    for(Character c : toPrint.toCharArray()) {
+        String word = "";
+        for(Character c : toPrint.toCharArray()) {
             if(! ignore.contains(c)) {
-               c.toString();
+                c.toString();
                 word= word+c;
             }
         }return word;
     }
 
 }
-
--- src/SemAnalizer.java
+++ src/SemAnalizer.java
@@ -1,3 +1,4 @@
+
 import com.sun.source.tree.Tree;
 
 import java.io.FileNotFoundException;
@@ -7,14 +8,46 @@ import java.util.TreeMap;
 
 public class SemAnalizer {
     static TreeMap<Integer, ArrayList<String>> maxWords = new TreeMap<>();
+    static TreeMap<Integer, ArrayList<String>> maxWordsWithLetterCnt = new TreeMap<>();
 
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println(findMaxWords(Reader.read("PairProject.From11.11/borbala.txt")));
+        System.out.println(findMaxWords(Reader.read("borbala.txt")));
+        System.out.println(findMaxWordsWithLetterCnt(Reader.read("borbala.txt"),1));
     }
-//2.    Gyűjtsd ki egy szöveg 10 leggyakoribb 2-3-4 szavas szófordulatát!
+    //2.    Gyűjtsd ki egy szöveg 10 leggyakoribb 2-3-4 szavas szófordulatát!
+    public static TreeMap<Integer, ArrayList<String>> findMaxWordsWithLetterCnt(HashMap<String, Integer> words,int letterCnt) {
+        int maxCnt = Integer.MAX_VALUE;
+        for (int i = 0; i < 10; ) {
+            Integer actuelMaxCnt = findMaxWordsUnderMaxCntWithLetterCnt(words, maxCnt,letterCnt);
+            System.out.println(actuelMaxCnt);
+            maxWordsWithLetterCnt.putIfAbsent(actuelMaxCnt, new ArrayList<>());
+            for (String s : words.keySet()) {
+                if (words.get(s).equals(actuelMaxCnt)  && s.toCharArray().length==(letterCnt)) {
+                    maxWordsWithLetterCnt.get(actuelMaxCnt).add(s);
+                    i++;
+                }
+            }
+            if (actuelMaxCnt<0){
+                break;}
+            maxCnt = actuelMaxCnt;
 
 
-//   1. Gyűjtsd ki egy szöveg 10 leggyakoribb szavát!
+        }
+        return maxWordsWithLetterCnt;
+    }
+
+    public static Integer findMaxWordsUnderMaxCntWithLetterCnt(HashMap<String, Integer> words, int underMaxCnt,int letterCnt) {
+        int max = Integer.MIN_VALUE;
+        for (String s : words.keySet()) {
+            if ((!s.equals("")) && words.get(s) > max && words.get(s) < underMaxCnt && s.toCharArray().length==(letterCnt) ) {
+                max = words.get(s);
+            }
+//            System.out.println(max);
+        }
+        return max;
+    }
+
+    //   1. Gyűjtsd ki egy szöveg 10 leggyakoribb szavát!
     public static TreeMap<Integer, ArrayList<String>> findMaxWords(HashMap<String, Integer> words) {
         int maxCnt = Integer.MAX_VALUE;
         for (int i = 0; i < 10; ) {
