3fe83b616f26d89cd96185ab8ff9c0bd09e84401
gercike <gercike13@gmail.com>
2020-11-11 15:11:03 +0100

    bullshitgenerator v1.0

--- src/BullShitGenerator.java
+++ src/BullShitGenerator.java
@@ -1,2 +1,27 @@
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class BullShitGenerator {
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<String> text = Reader.readToArray("borbala.txt");
+//        System.out.println(Reader.wordsHashMap.keySet().size());
+//        System.out.println(SemAnalizer.findMaxWords(Reader.wordsHashMap));
+        BullShitGenerator main = new BullShitGenerator();
+        System.out.println(main.bullshitGenerator(text));
+    }
+
+    public String bullshitGenerator(ArrayList<String> words) {
+        Random rndmzr = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < words.size(); i++) {
+            int nr = rndmzr.nextInt(words.size());
+            Set<Integer> nrsUsed = new HashSet<>();
+            nrsUsed.add(nr);
+            text.append(words.get(nr));
+            if (i != words.size() - 1) {
+                text.append(" ");
+            }
+        }
+        return text.toString();
+    }
 }
