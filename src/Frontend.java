232cd77579c2965d004b2938fe08f3f927e33869
gercike <gercike13@gmail.com>
2020-11-11 22:20:17 +0100

    frontend in progress

--- src/Frontend.java
+++ src/Frontend.java
@@ -3,7 +3,32 @@ import java.util.Scanner;
 public class Frontend {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Üdvözli a szövegelemző és halandzsagyártó alkalmazás!");
-        System.out.print("Melyik szolgáltatásunk érdekli? A szövegelemzés (1) vagy a halandzsagyártás (2)? ");
+        System.out.println("Üdvözli a szemantikai elemző és halandzsagyártó alkalmazás!");
+        String service = "0";
+        String textChoose = "0";
+        String textSource = "0";
+        while (!service.equals("1") && !service.equals("2")) {
+            System.out.print("Melyik szolgáltatásunk érdekli? A szemantikai elemzés (1) vagy a halandzsagyártás (2)? ");
+            service = s.next();
+            if (!service.equals("1") && !service.equals("2")) {
+                System.out.print("A felkínált lehetőségek (1 vagy 2) közül szíveskedjen választani!");
+            } else if (service.equals("1")) {
+                while (!textChoose.equals("1") && !textChoose.equals("2")) {
+                    System.out.print("A szemantikai elemzéshez Ön ad meg referencia-szöveget (1) vagy használjam a beépített referencia-szöveget (2)? ");
+                    textChoose = s.next();
+                    if (!textChoose.equals("1") && !textChoose.equals("2")) {
+                        System.out.print("A felkínált lehetőségek (1 vagy 2) közül szíveskedjen választani!");
+                    } else if (textChoose.equals("1")) {
+                        System.out.print("Szíveskedjen megadni a forrásszöveg elérési útját: ");
+                        textSource = s.next();
+                    } else {
+                        System.out.println("A szemantikai elemzéshez a beépített referencia-szöveget fogom használni.");
+                    }
+                }
+            } else {
+                System.out.print("Szíveskedjen megadni a forrásszöveg elérési útját: ");
+                textSource = s.next();
+            }
+        }
     }
 }
