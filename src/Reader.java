acfda015e3662ed9c5ed795206f1140b665ca047
Mateskoda <skodamate93@gmail.com>
2020-11-12 11:28:58 +0100

    can not read osszesffi and osszesnoi file

--- src/Reader.java
+++ src/Reader.java
@@ -16,55 +16,75 @@ public class Reader {
 //        System.out.println(SemAnalizer.findMaxWords(Reader.wordsHashMap));
 //        BullShitGenerator main = new BullShitGenerator();
 //        System.out.println(main.bullshitGenerator(text));
-        System.out.println(readNames("borbala.txt"));
+//        System.out.println(readNames("borbala.txt"));
+        System.out.println(readHunFirstNames());
     }
-public static void readHunFirstNames() {
-}
-    public static HashMap<String, Integer> readNames (String textName) throws FileNotFoundException {
-      Set<Character> sentenceEndingCharsToIgnore = new HashSet<>();
-      Set<Character> charsToIgnore = new HashSet<>();
-          sentenceEndingCharsToIgnore.add(('.'));
-          sentenceEndingCharsToIgnore.add(('?'));
-          sentenceEndingCharsToIgnore.add(('!'));
-          sentenceEndingCharsToIgnore.add(('-'));
+public static ArrayList<String> readHunFirstNames() throws FileNotFoundException {
+Scanner sc = new Scanner(new File("osszesffi.txt"));
+Scanner sc2 = new Scanner(new File("osszesnoi.txt"));
+ArrayList< String> names = new ArrayList<>();
+    for (int i = 0; i<0 ; i++) {
+        String name = sc.next();
+        names.add(name);
+        System.out.println(name);
+    }
 
-        for (int i = 33; i < 64; i++) {
-
-            charsToIgnore.add((char) i);
-        }
-      Scanner sc = new Scanner(new File(textName));
-
-
-          String word2 = sc.next();
-          for (int i = 0; sc.hasNext(); i++) {
-              String word = word2;
-              word2 = sc.next();
-              String wordSmallLetters = word.substring(0).toLowerCase();
-              String wordWithIgnoresString = wordWithIgnores(word, charsToIgnore);
-              if (!wordSmallLetters.equals(word)){
-                  wordsHashMapOfNames.putIfAbsent(wordWithIgnoresString, 0);
-//                  Integer put = wordsHashMap.get(word);
-//                  put++;
-//                  wordsHashMap.put(word, put);
-              }
-              word2 = word2.substring(0).toLowerCase();
-               char dots= word.toCharArray()[word.toCharArray().length-1];
-              if (sentenceEndingCharsToIgnore.contains(dots)){
-                  if (sc.hasNext()){
-                word2 = sc.next();}
-                  if (sc.hasNext()){
-                word2 = sc.next();}
-                      if (sc.hasNext()) {
-                          word2 = sc.next();}
-                      }
-              while ( word2.toCharArray()[0]==(char)10){
-                  word2=sc.next();
-                  word2=sc.next();
-              }
-
-      }
-      return wordsHashMapOfNames;
-  }
+    for (int i = 0; sc2.hasNextLine() ; i++) {
+        names.add(sc2.nextLine());
+        System.out.println(sc2.nextLine());
+    }return names;
+    }
+//    public static HashMap<String, Integer> readNames (String textName) throws FileNotFoundException {
+//      Set<Character> sentenceEndingCharsToIgnore = new HashSet<>();
+//      Set<Character> charsToIgnore = new HashSet<>();
+//          sentenceEndingCharsToIgnore.add(('.'));
+//          sentenceEndingCharsToIgnore.add(('?'));
+//          sentenceEndingCharsToIgnore.add(('!'));
+//          sentenceEndingCharsToIgnore.add(('-'));
+//
+//        for (int i = 33; i < 64; i++) {
+//
+//            charsToIgnore.add((char) i);
+//        }
+//      Scanner sc = new Scanner(new File(textName));
+//
+//
+//          String word2 = sc.next();
+//          for (int i = 0; sc.hasNext(); i++) {
+//              String word = word2;
+//              word2 = sc.next();
+//              String wordSmallLetters = word.substring(0).toLowerCase();
+//              String wordWithIgnoresString = wordWithIgnores(word, charsToIgnore);
+//              if (!wordSmallLetters.equals(word)){
+//                  wordsHashMapOfNames.putIfAbsent(wordWithIgnoresString, 0);
+////                  Integer put = wordsHashMap.get(word);
+////                  put++;
+////                  wordsHashMap.put(word, put);
+//              }
+//              word2 = word2.substring(0).toLowerCase();
+//               char dots= word.toCharArray()[word.toCharArray().length-1];
+//              if (sentenceEndingCharsToIgnore.contains(dots)){
+//
+//                  dots= word2.toCharArray()[0];
+////                  if (charsToIgnore.contains(dots)){
+////                      word2 = sc.next();}dots= word2.toCharArray()[0];
+////                  if (charsToIgnore.contains(dots)){
+////                      word2 = sc.next();}dots= word2.toCharArray()[0];
+////                  if (charsToIgnore.contains(dots)){
+////                      word2 = sc.next();}
+//                  if (sc.hasNext()){
+//                word2 = sc.next();}
+//                      if (sc.hasNext()) {
+//                          word2 = sc.next();}
+//                      }
+//              while ( word2.toCharArray()[0]==(char)10){
+//                  word2=sc.next();
+//                  word2=sc.next();
+//              }
+//
+//      }
+//      return wordsHashMapOfNames;
+//  }
 
     public static ArrayList<String> readToArray(String textName) throws FileNotFoundException {
         ArrayList<String> arrayListOfWords = new ArrayList<>();
